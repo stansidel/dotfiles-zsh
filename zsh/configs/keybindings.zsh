a3345e8cc5df926a2735347fae32d6cda55e5cfc
Stan Sidel <stansidel@gmail.com>
2022-02-05 18:50:18 +0500

    Search history with up and down arrows

--- zsh/configs/keybindings.zsh
+++ zsh/configs/keybindings.zsh
@@ -20,3 +20,11 @@ bindkey -s "^T" "^[Isudo ^[A" # "t" for "toughguy"
 bindkey -e
 bindkey "^[b" backward-word
 bindkey '^[f' forward-word
+
+# Search history with up arrows
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search # Up
+bindkey "^[[B" down-line-or-beginning-search # Down
